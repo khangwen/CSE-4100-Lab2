7f1df7818acd5a7967585863282ad020c3a9fea6
khangwen <a_wen197@yahoo.com>
2022-11-23 08:50:36 -0800

    made tcp connection with port 1000 issue

--- tcp_client-2.c
+++ tcp_client-2.c
@@ -4,29 +4,52 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netdb.h>
+
+#define BUF_SIZE 1024
 
 // Find Header Files and Put them here
 // For example, #include <stdio.h>
 
 // Please Add your remark at the ends of lines, line by line
 
-int main(){
+int main(int argc, char *argv[])
+{
   int sock;
   char message[BUF_SIZE];
   int str_len;
   struct sockaddr_in serv_adr;
 
-  sock=socket(PF_INET, SOCK_STREAM, 0);   
+  sock = socket(PF_INET, SOCK_STREAM, 0);
 
   memset(&serv_adr, 0, sizeof(serv_adr));
-  serv_adr.sin_family=AF_INET;
-  serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
-  serv_adr.sin_port=htons(atoi(argv[2]));
-    
-  connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) 	
+  serv_adr.sin_family = AF_INET;
+  serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_adr.sin_port = htons(atoi(argv[2]));
+
+  connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
 
   // Write a code to receive a hello message from server and show the hello on the terminal after the connection is established.
-  
+  char buff[80];
+  int n;
+
+  for (;;)
+  {
+    bzero(buff, sizeof(buff));
+    printf("Enter the message to send : ");
+    n = 0;
+    while ((buff[n++] = getchar()) != '\n')
+      ;
+    write(sock, buff, sizeof(buff));
+    bzero(buff, sizeof(buff));
+    read(sock, buff, sizeof(buff));
+    if ((strncmp(buff, "exit", 4)) == 0)
+    {
+      printf("Exiting...\n");
+      break;
+    }
+  }
+
   close(sock);
 
   return 0;
--- tcp_server-2.c
+++ tcp_server-2.c
@@ -4,13 +4,15 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#define BUF_SIZE 1024
 
 // Find Header Files and Put them here
 // For example, #include <stdio.h>
 
 // Please Add your remark at the ends of lines, line by line
 
-int main(){
+int main(int argc, char *argv[])
+{
   int serv_sock, clnt_sock;
   char message[BUF_SIZE];
   int str_len, i;
@@ -18,21 +20,42 @@ int main(){
   struct sockaddr_in clnt_adr;
   socklen_t clnt_adr_sz;
 
-  serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
   memset(&serv_adr, 0, sizeof(serv_adr));
-  serv_adr.sin_family=AF_INET;
-  serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-  serv_adr.sin_port=htons(atoi(argv[1]));
-  bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
-  listen(serv_sock, 5); 	
-  clnt_adr_sz=sizeof(clnt_adr);
-  clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-  
+  serv_adr.sin_family = AF_INET;
+  serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_adr.sin_port = htons(atoi(argv[1]));
+  bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
+  listen(serv_sock, 5);
+  clnt_adr_sz = sizeof(clnt_adr);
+  clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+
   // Write a code to send a hello message to clinet after the connection is established.
+  char buff[80];
+  int n;
+
+  for (;;)
+  {
+    bzero(buff, 80);
+
+    read(clnt_sock, buff, sizeof(buff));
+
+    printf("Client message : %s\t Send to client : ", buff);
+    bzero(buff, 80);
+    n = 0;
+    while ((buff[n++] = getchar()) != '\n')
+      ;
+    write(clnt_sock, buff, sizeof(buff));
+
+    if (strncmp("exit", buff, 4) == 0)
+    {
+      printf("Server Exiting...\n");
+      break;
+    }
+  }
 
   close(clnt_sock);
   close(serv_sock);
 
   return 0;
-
 }
